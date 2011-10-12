c33673ee6f9a9f75d0042539c54cb9743ae57015
Victor Martinez <pitillo@ono.com>
2011-10-12 14:56:10 +0000

    Updated vars.mk

--- vars.mk
+++ vars.mk
@@ -3,7 +3,7 @@
 #
 
 HOST = $(shell echo $$MACHTYPE | sed "s/$$(echo $$MACHTYPE | cut -d- -f2)/cross/")
-TARGET = arm-crux-linux-gnueabi
+TARGET = arm-crux-linux-gnueabihf
 
 PWD  = $(shell pwd)
 CLFS = $(PWD)/clfs
@@ -11,11 +11,11 @@ CROSSTOOLS = $(PWD)/crosstools
 WORK = $(PWD)/work
 
 KERNEL_HEADERS_VERSION = 2.6.35.6
-LIBGMP_VERSION = 5.0.1
-LIBMPFR_VERSION = 3.0.0
-LIBMPC_VERSION = 0.8.2
-BINUTILS_VERSION = 2.20.1
-GCC_VERSION = 4.5.2
-GLIBC_VERSION = 2.12.1
+LIBGMP_VERSION = 5.0.2
+LIBMPFR_VERSION = 3.1.0
+LIBMPC_VERSION = 0.9
+BINUTILS_VERSION = 2.21.1
+GCC_VERSION = 4.6.1
+GLIBC_VERSION = 2.13
 
 # End of file
